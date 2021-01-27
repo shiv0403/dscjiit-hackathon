e60c2dec89c9426085816c319c3dcba04ef95d42
Shivansh Gupta <shiv0403gupta@gmail.com>
2021-01-27 14:12:58 +0530

    added faqs section

---
+++ .history/js/faqs_20210127132042.js
@@ -0,0 +1,20 @@
+var coll = document.getElementsByClassName("collapsible");
+
+for (var i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    // this.classList.toggle("faqs_active");
+    var content = this.nextElementSibling;
+    content.classList.add("faqs__content__active");
+    if (content.style.maxHeight != 0) {
+      content.style.maxHeight = null;
+    } else {
+      for (var j = 0; j < coll.length; ++j) {
+        if (coll[j].nextElementSibling.style.maxHeight != 0) {
+          coll[j].nextElementSibling.style.maxHeight = null;
+          break;
+        }
+      }
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
